64ca798565a225b7b4ba72c3baef2257403b90b6
SyconX <javierfraga10@gmail.com>
2022-12-17 18:10:05 +0100

    cambios en inicio

--- app/views/home.php
+++ app/views/home.php
@@ -24,39 +24,33 @@
         </a>
     </div>
     <div class="text">
-        <h5> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet libero sit amet odio convallis
-            dictum a vel sem. Fusce tempus metus orci, id accumsan arcu pharetra nec. Morbi et orci felis. Quisque
-            dignissim dui sapien, eu scelerisque est convallis nec. Sed facilisis pellentesque dignissim.
-            Pellentesque
-            lacus augue, faucibus eget ante eu, hendrerit ullamcorper ex. Nullam et iaculis nisi, sed condimentum
-            dolor.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In a metus
-            lacinia, maximus nibh ac, tincidunt sapien. Curabitur sodales purus dolor, id fringilla dui feugiat a.
-        </h5>
+        <h4> 
+        Planea tus partidas, organiza eventos y aprovisiónate de los mejores juegos
+        </h4>
+        <h4> 
+        ¡Xoga Hoxe!
+        </h4>
+        
     </div>
     <div class="parallax second"></div>
     <div class="text">
-        <h5> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet libero sit amet odio convallis
-            dictum a vel sem. Fusce tempus metus orci, id accumsan arcu pharetra nec. Morbi et orci felis. Quisque
-            dignissim dui sapien, eu scelerisque est convallis nec. Sed facilisis pellentesque dignissim.
-            Pellentesque
-            lacus augue, faucibus eget ante eu, hendrerit ullamcorper ex. Nullam et iaculis nisi, sed condimentum
-            dolor.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In a metus
-            lacinia, maximus nibh ac, tincidunt sapien. Curabitur sodales purus dolor, id fringilla dui feugiat a.
-        </h5>
+        <h4> ¿Te faltan jugadores para estrenar tu ejemplar de la Herencia de Tia Agata?
+        </h4>
+        <h4> ¿Tienes claro tu juego pero no encuentras gente?
+        </h4>
+        <h4> ¿Tus amigos son más de playa?
+        </h4>
+        <h4> Haz planes, juega y conoce gente en Xoga Hoxe, tu comunidad de juegos.
+        </h4>
     </div>
     <div class="parallax third"></div>
     <div class="text">
-        <h5> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet libero sit amet odio convallis
-            dictum a vel sem. Fusce tempus metus orci, id accumsan arcu pharetra nec. Morbi et orci felis. Quisque
-            dignissim dui sapien, eu scelerisque est convallis nec. Sed facilisis pellentesque dignissim.
-            Pellentesque
-            lacus augue, faucibus eget ante eu, hendrerit ullamcorper ex. Nullam et iaculis nisi, sed condimentum
-            dolor.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In a metus
-            lacinia, maximus nibh ac, tincidunt sapien. Curabitur sodales purus dolor, id fringilla dui feugiat a.
-        </h5>
+        <h4> Soy jugador asiduo de juegos de mesa de todos los estilos, colores y sabores, con amigos y familia. Desde que soy pequeño he jugado a una gran variedad de juegos.
+        </h4>
+        <h4> Mi ilusión es que disfrutes de este proyecto tanto como yo he disfrutado de todas esas partidas al Risk, Monopoly, ajedrez, oca y parchís.
+        </h4>
+        <h4> (Obviamos los juegos de cartas, a los que siempre me ganaba mi madre)
+        </h4>
     </div>
     <?php if (!isset($_SESSION['userLogin'])) { ?>
     <div class="container" id="formLogin">
